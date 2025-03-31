106246de8794f9d679f409fe2c5ad0f3bf14e40f
michbogos <bogostamicheal@gmail.com>
2025-03-31 13:03:37 +0200

    Passing uniforms

--- src/shader.fs
+++ src/shader.fs
@@ -1,8 +1,10 @@
 $input v_color0
 
+uniform vec4 time[1];
+
 #include "../_deps/bgfx-src/bgfx/src/bgfx_shader.sh"
 
 void main()
 {
-	gl_FragColor = v_color0;
+	gl_FragColor = v_color0*abs(sin(time[0].x))+0.5;
 }
